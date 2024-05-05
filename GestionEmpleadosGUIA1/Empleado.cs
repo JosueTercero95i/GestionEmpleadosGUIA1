01aa2dc49b766739e4b4bb329c2d52e19c8b143e
JosueTercero95i <167829608+JosueTercero95i@users.noreply.github.com>
2024-05-05 16:33:23 -0600

    first

---
+++ GestionEmpleadosGUIA1/Empleado.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GestionEmpleadosGUIA1
+{
+    internal class Empleado
+    {
+        private string nombre;
+        private int numeroempleado;
+        private decimal salario;
+        private bool activo;
+
+        public Empleado(string nombre, int numeroempleado, decimal salario, bool activo = true)
+        {
+            this.nombre = nombre;
+            this.numeroempleado = numeroempleado;
+            this.salario = salario;
+            this.activo = activo;
+        }
+
+        public void MostrarDetalles()
+        {
+            string estado = (activo) ? "Activo" : "Inactivo";
+            Console.WriteLine($"Nombre: {nombre}, Numero Empleado: {numeroempleado}, Salario: {salario:C}, Estado: {estado}");
+        }
+
+        public int ObtenerNumeroEmpleado()
+        {
+            return this.numeroempleado;
+        }
+
+        public void CambiarEstado(bool NuevoEstado)
+        {
+            if (NuevoEstado)
+            {
+                activo = true;
+                Console.WriteLine("Empleado Activado");
+            }
+            else
+            {
+                activo = false;
+                Console.WriteLine("Empleado Desactivado");
+            }
+        }
+
+        public static Empleado BuscarEmpleado(List<Empleado> empleados, int NumeroEmpleado)
+        {
+            foreach (var emp in empleados)
+            {
+                if (emp.ObtenerNumeroEmpleado() == NumeroEmpleado)
+                {
+                    return emp;
+                }
+            }
+            return null;
+        }
+
+   
+    }
+}
---
+++ GestionEmpleadosGUIA1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GestionEmpleadosGUIA1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aplicacion de gestion de empleados");
+
+            // Creación de una lista de empleados
+            List<Empleado> ListaEmpleados = new List<Empleado>();
+
+            ListaEmpleados.Add(new Empleado("Juan", 101, 30000));
+            ListaEmpleados.Add(new Empleado("Maria", 102, 35000));
+            ListaEmpleados.Add(new Empleado("Pedro", 103, 32000, false));
+
+            foreach (var empleado in ListaEmpleados)
+            {
+                empleado.MostrarDetalles();
+            }
+
+            Console.ReadLine(); 
+        }
+    }
+}
